58c2f360fde736181ee68144434f8bcb7eb548dd
Raghav Sharma <redclaww02@gmail.com>
2023-10-24 19:53:47 +0530

    Added website

---
+++ Sushi Website/Sushiman/js/script.js
@@ -0,0 +1,53 @@
+// import images as relative image path won't work with vite/vercel.
+
+import sushi12 from '../assets/sushi-12.png'
+import sushi11 from '../assets/sushi-11.png'
+import sushi10 from '../assets/sushi-10.png'
+
+
+// import "aos/dist/aos.css";
+
+
+
+const trendingSushis = [
+    'Make Sushi',
+    'Nigiri Sushi',
+    'Oshizushi',
+    'Temaki Sushi',
+    'Uramaki Sushi',
+    'Inari Sushi'
+];
+
+const trendingDrinks = [
+    "Oruncha",
+    "Ofukucha",
+    "Sakura Tea",
+    "Kombu-cha",
+    "Aojiru",
+    "Mugicha",
+]
+
+const cards = [
+    {
+        imgSrc: sushi12,
+        alt: "sushi-12",
+        title: "Chezu Sushi",
+        rating: "4.8",
+        price: "$21.00"
+    },
+    {
+        imgSrc: sushi11,
+        alt: "sushi-11",
+        title: "Originale Sushi",
+        rating: "4.8",
+        price: "$21.00",
+        active: true
+    },
+    {
+        imgSrc: sushi10,
+        alt: "sushi-10",
+        title: "Ramen Legendo",
+        rating: "4.8",
+        price: "$21.00"
+    }
+];
